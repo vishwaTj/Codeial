5790ad1c5abb4708d02f575a440b1894efd182cc
Tj_ <tjvishwa88@gmail.com>
2023-01-24 10:35:20 +0530

    Fetch data from the API using global fetch function from FETCH API

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { getPosts } from "../api";
 
 function App() {
+  // calling the fetch function using useEffect
+  useEffect(async ()=>{
+    const fetchPosts = async()=>{
+      const response = await getPosts();
+      console.log('response',response);
+    }
+    
+    fetchPosts();
+  },[]);
   return (
       <div className="App">
         <h1>Hello World</h1>
